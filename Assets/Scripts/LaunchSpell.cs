fc879b348aed9e8ce6cd5d8ebab8ed0902c33625
Ozair Zaman <ozair.zaman@outlook.com>
2019-11-20 10:26:36 +1100

    Added a few effects

---
+++ Assets/Scripts/LaunchSpell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchSpell : MonoBehaviour
+{
+    #region Variables
+    Rigidbody rigid;
+    public float force = 100;
+    #endregion
+
+    #region Internal
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        rigid.AddForce(new Vector3(0, 0, force), ForceMode.Acceleration);
+    }
+    #endregion
+
+
+}
